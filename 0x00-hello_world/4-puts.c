df46af5e5e6508b7037bebd8cbb146658cdfd0e1
le3enda <le3enda@gmail.com>
2023-09-07 16:51:23 -0700

    aaa

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *Main this is main c program
+ *descripation
+ *Retrun 0 alawys maen scusse
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
