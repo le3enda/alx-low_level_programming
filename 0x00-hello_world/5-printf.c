8851bc9c66093407f0c81b95f09a455aeb545f2a
le3enda <le3enda@gmail.com>
2023-09-08 03:34:00 -0700

    hello c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 /**
- *miain - intery point.                          *                                               *description print with printf function.
- *                                               *Return: alaways 0 (succsess).
+ * main - intizaling pount.
+ *
+ * Description : A C program for printf functio.
+ *
+ * Retrun :always  0 scuccses.
 */
 
 int main(void)
