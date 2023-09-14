2c8b4f5839dd0c05489b8e3c8cdc9f24e7c81d5c
le3enda <le3enda@gmail.com>
2023-09-14 15:42:03 -0700

    deg c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -16,15 +16,10 @@ printf("Infinite loop incoming :(\n");
 i = 0;
 
 /*while (i < 10)*/
-
 /*{*/
-
 /*putchar(i);*/
-
 /*}*/
 
-printf("Infinite loop avoided! \\o/\n");
-
+printf("Infinite l7oop avoided! \\o/\n");
 return (0);
-
 }
