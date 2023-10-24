b6c2d70d7d1b57d37123d9827dcd7c0dc7c06e4d
le3enda <le3enda@gmail.com>
2023-10-24 15:37:03 -0700

    hh

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,15 +8,14 @@
  */
 int sum_listint(listint_t *head)
 {
- int sum = 0;
- listint_t *temp = head;
+int sum = 0;
+listint_t *temp = head;
 
- while (temp)
- {
- sum += temp->n;
- temp = temp->next;
- }
-
- return (sum);
+while (temp)
+{
+sum += temp->n;
+temp = temp->next;
 }
 
+return (sum);
+}
