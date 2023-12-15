e6d0d7cb237df959f9a2607937aee3462fa3414a
le3enda <le3enda@gmail.com>
2023-12-15 12:02:20 -0800

    h

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in h
+ */
+
+int main(void)
+{
+int add(int a, int b)
+{
+	return a + b;
+}
+int sub(int a, int b)
+{
+	return a - b;
+}
+int mul(int a, int b)
+{
+	return a * b;
+}
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("erorr: div by zero\n");
+		return 0;
+	}
+	return a / b;
+}
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+	printf("erorr: div by zero\n");
+	return 0;
+	}
+	return a % b;
+}
+}
